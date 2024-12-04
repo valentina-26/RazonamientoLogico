8b9225fac0c44edf67a93f4c3996d2d01b86638d
valentina-26 <160797346+valentina-26@users.noreply.github.com>
2024-12-04 18:23:16 -0500

    feat: :white_check_mark: Ejercicio 4

--- Ejercicios.py
+++ Ejercicios.py
@@ -1,21 +1,18 @@
-# Escriba un programa que muestre los m primeros numeros de Fibonacci,donde m es un numero ingresado por el usuario:
+# Función para calcular la suma de divisores propios (excluyendo al número)
+def suma_divisores(numero):
+    suma = 0
+    for i in range(1, numero):
+        if numero % i == 0:
+            suma += i
+    return suma
 
-m = int(input("Por favor inngrese m: "))
-
-a = 0
-b = 1
-
-if m >= 1:
-    print(a, end=" ")
-if m >= 2:
-    print(b, end=" ")
-
-for i in range(2, m):
-    c = a + b
-    print(c, end=" ")
-    a = b
-    b = c
-
-print()
+# Función principal para buscar números amigos en el rango [1000, 1500]
+def encontrar_numeros_amigos():
+    for m in range(1000, 1500):
+        n = suma_divisores(m)  # Calcula la suma de divisores de m
+        if n > m and n <= 1500:  # Asegura que n esté dentro del rango y sea mayor que m
+            if suma_divisores(n) == m:  # Verifica si son amigos
+                print(f"Par de números amigos encontrado: {m} y {n}")
 
 
+encontrar_numeros_amigos()
